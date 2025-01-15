fcc7088092b1f39a8d1515c7c4920ea11a01d1b4
Jishnu Duhan <duhanjishnu@gmail.com>
2025-01-15 17:14:30 +0530

    Edit home add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-import Image from "next/image";
+import Navigation from '@/components/navigation'
+import MainScene from '@/components/main-scene'
+import HeadliningEvents from '@/components/head-events'
 
 export default function Home() {
   return (
-    <div>Shishir 2025</div>
-  );
+    <main>
+      <Navigation />
+      <div className='home_contianer relative border-box overflow-hidden'>
+        <MainScene />
+        <HeadliningEvents />
+      </div>
+      
+    </main>
+  )
 }
