010e2e362136f6a58341dcb994d229a356b0bbda
Charity Rymbai <charityrymbai@hotmail.com>
2025-01-16 15:44:14 +0530

    fix some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Navigation from '@/components/homepage/navigation'
 import MainScene from '@/components/homepage/main-scene'
 import HeadliningEvents from '@/components/homepage/head-events'
 import About from '@/components/homepage/about'
@@ -6,7 +5,6 @@ import About from '@/components/homepage/about'
 export default function Home() {
   return (
     <main>
-      <Navigation />
       <div className='home_contianer relative border-box overflow-hidden'>
         <MainScene />
         <HeadliningEvents />
