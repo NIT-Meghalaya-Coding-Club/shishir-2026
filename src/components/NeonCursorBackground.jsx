3e36106d291464b2f8d899e69dec6966dd1b51b8
Jishnu Duhan <duhanjishnu@gmail.com>
2025-02-07 10:29:53 +0530

    add neon cursor bg

---
+++ src/components/NeonCursorBackground.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { neonCursor } from 'threejs-toys';
+
+const NeonCursorBackground = () => {
+  useEffect(() => {
+    neonCursor({
+      el: document.getElementById('app'),
+      shaderPoints: 16,
+      curvePoints: 80,
+      curveLerp: 0.5,
+      radius1: 5,
+      radius2: 30,
+      velocityTreshold: 10,
+      sleepRadiusX: 100,
+      sleepRadiusY: 100,
+      sleepTimeCoefX: 0.0025,
+      sleepTimeCoefY: 0.0025,
+    });
+  }, []);
+
+  return (
+    <div id="app" className="absolute w-full h-full  overflow-hidden touch-pan-up">
+    </div>
+  );
+};
+
+export default NeonCursorBackground;
