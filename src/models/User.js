14db498dad3e11b1c7b469a499a2ccc0aa75c1c7
webgaurav02 <gauravcodes123@gmail.com>
2025-02-09 20:56:50 +0530

    Updated User model

--- src/models/User.js
+++ src/models/User.js
@@ -4,15 +4,11 @@ const { Schema } = mongoose;
 // User Schema
 const userSchema = new Schema(
   {
-    id: {
-      type: String,
-      default: () => new mongoose.Types.ObjectId().toString(),
-      unique: true,
-    },
     name: { type: String, default: null },
     email: { type: String, unique: true, required: true },
-    emailVerified: { type: Date, default: null },
-    password: { type: String, default: null },
+    phone: { type: String, unique: true },
+    accomdation: { type: Boolean, default: false },
+    nonVeg: { type: Boolean, default: false },
     image: { type: String, default: null },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
