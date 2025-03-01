ee780359e6f0b4dbc9eeaab6de376e4036c6029b
webgaurav02 <gauravcodes123@gmail.com>
2025-03-01 19:03:29 +0530

    Fixed redirect link on Logout

--- src/components/dashboard/LogoutButton.tsx
+++ src/components/dashboard/LogoutButton.tsx
@@ -4,7 +4,7 @@ import { signOut } from "next-auth/react";
 
 const MoreButton = () => {
   const handleLogout = () => {
-    signOut();
+    signOut({ callbackUrl: "/register" });
   };
 
   return (
