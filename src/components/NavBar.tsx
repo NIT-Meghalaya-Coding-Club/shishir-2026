15cb09d61a24270009e1f93fe862fb68eec2dfae
Ankit2533 <singhankit1833@gmail.com>
2025-03-02 01:04:59 +0530

    hiding schedule for now

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -88,7 +88,7 @@ const NavBar: React.FC = () => {
             <NavBarItem to="/" text="Home" onClick={closeMenu} />
             <NavBarItem to="/events" text="Events" onClick={closeMenu} />
             {/* <NavBarItem to="/competitions" text="Competitions" onClick={closeMenu} /> */}
-            <NavBarItem to="/schedule" text="Schedule" onClick={closeMenu} />
+            {/* <NavBarItem to="/schedule" text="Schedule" onClick={closeMenu} /> */}
             <NavBarItem to="/mun" text="MUN" onClick={closeMenu} />
             <NavBarItem to="/past-sponsors" text="Past Sponsors" onClick={closeMenu} />
             <NavBarItem to="/team" text="Team" onClick={closeMenu} />
