fd66e0a150bcab407aeef176c694694582c21062
Ankit2533 <singhankit1833@gmail.com>
2025-03-01 16:41:47 +0530

    build error solved + analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +32,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClientProviders>{children}</ClientProviders>
+        <Analytics/>
+        <ClientProviders>
+          {children}
+        </ClientProviders>
       </body>
     </html>
   );
