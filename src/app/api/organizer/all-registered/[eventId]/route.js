5eb269941138b5fea576b4654d83ba7edca49e71
Ankit2533 <singhankit1833@gmail.com>
2025-03-06 01:11:21 +0530

    remove debug

--- src/app/api/organizer/all-registered/[eventId]/route.js
+++ src/app/api/organizer/all-registered/[eventId]/route.js
@@ -1,20 +1,29 @@
-import connectMongo from '@/lib/mongodb';
-import Registration from '@/models/Registration';
+import connectMongo from "@/lib/mongodb";
+import Registration from "@/models/Registration";
 
 export async function GET(req, { params }) {
-    console.log("Params:", params); // Debug: Log the params object
-    try {
-      await connectMongo();
-      const { eventId } = params;
-  
-      if (!eventId) {
-        return new Response(JSON.stringify({ success: false, error: 'Event ID is required' }), { status: 400 });
-      }
-  
-      const registration = await Registration.find({ eventId }).sort({ createdAt: -1 });
-      return new Response(JSON.stringify({ success: true, registration }), { status: 200 });
-    } catch (error) {
-      console.error(error);
-      return new Response(JSON.stringify({ success: false, error: 'Server Error' }), { status: 500 });
+  try {
+    await connectMongo();
+    const { eventId } = params;
+
+    if (!eventId) {
+      return new Response(
+        JSON.stringify({ success: false, error: "Event ID is required" }),
+        { status: 400 }
+      );
     }
+
+    const registration = await Registration.find({ eventId }).sort({
+      createdAt: -1,
+    });
+    return new Response(JSON.stringify({ success: true, registration }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(
+      JSON.stringify({ success: false, error: "Server Error" }),
+      { status: 500 }
+    );
   }
+}
