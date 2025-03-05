84a3a3e29eafc7037cef805919cd24eaedab7111
Ankit2533 <singhankit1833@gmail.com>
2025-03-06 03:41:07 +0530

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://shishir.nitm.ac.in',
+    generateRobotsTxt: true,
+    exclude: ['/admin/*', '/private/*', '/organizer/*'],
+  }
