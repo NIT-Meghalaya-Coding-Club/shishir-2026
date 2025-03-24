114cb390114714d52b1d470477c6d264b4e40e82
Ankit2533 <singhankit1833@gmail.com>
2025-03-25 03:25:54 +0530

    tickets -> ticket

---
+++ src/app/ticket/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ShishirTicketSeller from "../../components/tickets/ticket";
+
+
+export default function TicketsPage() {
+  return (
+    <main className="h-screen w-full">
+      <ShishirTicketSeller />
+    </main>
+  );
+}
