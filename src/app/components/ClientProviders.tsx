56745fdfd8b04bc824e728ab0a41ee4889354177
Charity Rymbai <charityrymbai@hotmail.com>
2026-09-12 00:49:00 +0530

    removed the legacy organiser dashboard and related routes

--- src/app/components/ClientProviders.tsx
+++ src/app/components/ClientProviders.tsx
@@ -2,9 +2,6 @@
 
 import { SessionProvider } from "next-auth/react";
 
-//Context
-import { OrganizerProvider } from "@/context/OrganizerContext";
-
 //Components
 import Footer from "@/components/Footer";
 import NavBar from "@/components/NavBar";
@@ -14,12 +11,10 @@ import { ToastContainer } from "react-toastify";
 export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <OrganizerProvider>
-        <ToastContainer position="top-center" />
-        <NavBar />
-        {children}
-        <Footer />
-      </OrganizerProvider>
+      <ToastContainer position="top-center" />
+      <NavBar />
+      {children}
+      <Footer />
     </SessionProvider>
   );
 }
